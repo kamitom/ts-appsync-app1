e43e368414b15ad463caec772117a3f7c42ae6d0
Tom Chen <tom@itri.org.tw>
2019-10-16 17:08:45 +0800

    first commit for successful deploy

---
+++ functions/handler.ts
@@ -0,0 +1,38 @@
+import { Handler } from 'aws-lambda';
+import 'source-map-support/register';
+
+
+type MeEvent = {
+  userId: string;
+  username: string;
+};
+
+type MeResult = {
+  id: string;
+  name: string;
+  email?: string;
+  birthday?: string;
+  __typename: string;
+};
+
+
+export const hellome: Handler<MeEvent, MeResult> = async (event, _context) => {
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }, null, 2),
+  // };
+
+  const { userId, username } = event;
+
+  return {
+    id: '007',
+    name: 'tom jr',
+    email: 'tom@pm.me',
+    birthday: '83.5.23',
+    __typename: userId + username
+  }
+
+}
